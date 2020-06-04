07cc806146cf1efb227a3a60a81dfb39df4968df
sage-jordan <sagemjordan@gmail.com>
2020-06-03 22:53:41 -0400

    binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,8 +7,20 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
 
-    # Your code here
-
-
-    return -1  # not found
+    while low <= high:
+        mid = (high + low)
+        # Check if target is found
+        if arr[mid] < target:
+            low = mid + 1
+        # If target is greater, ignore left half
+        elif arr[mid] > target:
+            high = mid - 1
+        # If target is smaller, ignore right half
+        else:
+            return mid
+    # If we reach here, then the element was not found
+    return -1  
